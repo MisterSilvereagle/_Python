fe2ab3fe2f162c6a5f9ae95bce189c0323446694
MisterSilvereagle <MisterSilvereagle@gmx.de>
2022-06-11 23:27:30 +0200

    Worked on the "AngleDate" generation in gradient_angle.py

--- gradient_angle.py
+++ gradient_angle.py
@@ -1,11 +1,11 @@
 from PIL import Image
-from math import cos, sin, asin, acos, pi
+from math import cos, sin, pi
 from tqdm import tqdm
 
 colorA = "#32cd32"  # first color
 colorB = "#ff32cd"  # second color
-angle  = 45  # angle of the gradient in degrees
-width  = 1920  # width of gradient
+angle = 45  # angle of the gradient in degrees
+width = 1920  # width of gradient
 height = 1080  # height of gradient
 
 width = int(width/2)
@@ -14,6 +14,31 @@ height = int(height/2)
 list_imgs = []
 
 save_gif = 0
+
+
+def rgb2cmyk(color: list):
+    r = color[0]
+    g = color[1]
+    b = color[2]
+    _R = r / 255
+    _G = g / 255
+    _B = b / 55
+    K = 1 - max(_R, _G, _B)
+    C = (1 - _R - K) / (1 - K)
+    M = (1 - _G - K) / (1 - K)
+    Y = (1 - _B - K) / (1 - K)
+    return [int(C * 100), int(M * 100), int(Y * 100), int(K * 100)]
+
+
+def cmyk2rgb(color: list):
+    _C = color[0]
+    _M = color[1]
+    _Y = color[2]
+    _K = color[3]
+    R = 255 * (1 - _C) * (1 - _K)
+    G = 255 * (1 - _M) * (1 - _K)
+    B = 255 * (1 - _Y) * (1 - _K)
+    return [R, G, B]
 
 
 def hex2rgb(color: str):
@@ -41,50 +66,57 @@ def hex2rgba(color: str):
     return [__red, __green, __blue, __alpha]
 
 
-def mix2colors(colorA: list, colorB: list, amount: float):
+def mix2colors_rgb(colorA: list, colorB: list, amount: float):
     return [int((colorA[0] * (1 - amount) + colorB[0] * amount) + 0.5),
             int((colorA[1] * (1 - amount) + colorB[1] * amount) + 0.5),
             int((colorA[2] * (1 - amount) + colorB[2] * amount) + 0.5)]
 
 
+def mix2colors_cmyk(colorA: list, colorB: list, amount: float):
+    return [int((colorA[0] * (1 - amount) + colorB[0] * amount) + 0.5),
+            int((colorA[1] * (1 - amount) + colorB[1] * amount) + 0.5),
+            int((colorA[2] * (1 - amount) + colorB[2] * amount) + 0.5),
+            int((colorA[3] * (1 - amount) + colorB[3] * amount) + 0.5)]
+
+
 def calculateAngleData(angle):
     angle %= 360
-    _a = -0.5*cos((2*pi * angle / 360) - (0 * pi / 4))+0.5
-    # _b = 2 * asin(cos(pi * (angle / 180 - 0.5))) / pi
-    _b = sin(pi*angle / 360)
-    # _c = 2 * asin(cos(pi * angle / 180)) / pi
-    _c = sin(pi*(angle / 360 + 0.5))
+    _a = sin(pi * angle / 180)
+    _b = sin(pi * (angle / 180 + 0.5))
+    _c = 1 / max(abs(cos(pi * angle / 180)), abs(sin(pi * angle / 180)))
     return [_a, _b, _c]
 
 
 def calculateAmount(a, b, c, x, y):
-    return a + (((x / width) - 0.5) * b + (y / height) * c)
+    return (((x / width) - 0.5) * a + ((y / height) + 0.5) * b) / c
 
 
-colorA = hex2rgb(colorA)
-colorB = hex2rgb(colorB)
-for i in tqdm(range(0, 360, 1)):
-    a, b, c = calculateAngleData(i)
+if __name__ == "__main__":
+    colorA = hex2rgb(colorA)
+    colorB = hex2rgb(colorB)
+    for i in tqdm(range(0, 360, 45)):
+        a, b, c = calculateAngleData(i)
 
-    g_img = Image.new(mode="RGBA", size=(width, height))
+        g_img = Image.new(mode="RGBA", size=(width, height))
 
-    for x in range(width):
-        for y in range(height):
-            g_img.putpixel((x, y), tuple(mix2colors(colorA, colorB, calculateAmount(a, b, c, x, y))))
+        for x in range(width):
+            for y in range(height):
+                g_img.putpixel((x, y), tuple(mix2colors_rgb(colorA, colorB, calculateAmount(a, b, c, x, y))))
 
-    # g_img.show()
-    list_imgs.append(g_img)
+        # g_img.show()
+        list_imgs.append(g_img)
 
-    del g_img
+        del g_img
 
-if save_gif:
-    list_imgs.pop(0).save("./images/img.gif", format="GIF", append_images=list_imgs, save_all=True, duration=40, loop=0)
-else:
-    for img in range(len(list_imgs)):
-        number = '{:>03}'.format(img)
-        list_imgs[img].save(f'./images/{number}.png')
+    if save_gif:
+        list_imgs.pop(0).save("./images/img.gif", format="GIF", append_images=list_imgs, save_all=True, duration=40,
+                              loop=0)
+    else:
+        for img in range(len(list_imgs)):
+            number = '{:>03}'.format(img)
+            list_imgs[img].save(f'./images/{number}.png')
 
-from sys import exit as baum;baum()
+    from sys import exit as baum;baum()
 
 # top2botton            |   0,    0,    1 | x, sin(angle), cos(angle)
 # lefttop2rightbotton   |   0,  0.5,  0.5 |
